c9165cff8fb229e1933d3ba8f09d11a18939e8d9
Mikael Karon <mikael@karon.se>
2017-12-19 13:27:12 +0100

    make 'is' helper callable as a block

--- helpers/is.js
+++ helpers/is.js
@@ -1,1 +1,10 @@
-module.exports = Object.is;
+module.exports = (a, b, options) => {
+  // compare
+  var is = Object.is(a, b);
+  // check if we're called as a block
+  return options.fn && options.inverse
+    ? is
+      ? options.fn(this)
+      : options.inverse(this)
+    : is;
+};
